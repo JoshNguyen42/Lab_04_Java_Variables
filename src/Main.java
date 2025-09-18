d14d17d1d91d2cd3d5eeb44372aa77f55a080ef5
JoshNguyen42 <nguye2jc@mail.uc.edu>
2025-09-18 14:33:10 -0400

    part one

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,13 @@
 public class Main {
+    public static void main(String[] args) {
+        int intOperandA = 5;
+        int intOperandB = 10;
+        int intSum = intOperandA + intOperandB;
+        System.out.println("The sum of " + intOperandA + " and " + intOperandB + " is: " + intSum);
+
+        double doubleOperandA = 5.5;
+        double doubleOperandB = 10.5;
+        double doubleSum = doubleOperandA + doubleOperandB;
+        System.out.println("The sum of " + doubleOperandA + " and " + doubleOperandB + " is: " + doubleSum);
+    }
 }
